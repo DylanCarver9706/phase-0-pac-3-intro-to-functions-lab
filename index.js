486077a0a856e8133db3b993cb956aa40606f92d
Dylan <dylancarver14@gmail.com>
2022-06-11 00:41:32 -0500

    commit tree 5978917467d6323f3d617bf07eac0fa3eb848b0f

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string){
+    return string.toLowerCase()
+  }
+  
+  function logShout(string){
+    string = 'hello'
+    
+    console.log(string.toUpperCase())
+  }
+  
+  function logWhisper(string){
+    string = 'HELLO'
+    
+    console.log(string.toLowerCase())
+  }
+  function sayHiToHeadphonedRoommate (string) {
+    var 'hello' = "I can/'t hear you!";
+    var 'HELLO' = "YES INDEED!";
+    var "Let's have dinner together!" = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+      return "I can't hear you!";
+    }
+    else if (string.toUpperCase(string) === string) {
+      return "YES INDEED!";
+    }
+    else if ("Let's have dinner together!" === string) {
+      return "I would love to!"
+    }
